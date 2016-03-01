e583b9e441430a4289eed1454a739767cd00264f
James Pettit <etherealmachine@gmail.com>
2016-03-01 00:02:22 -0800

    Initial import.

---
+++ README.md
@@ -0,0 +1,5 @@
+Papertype is a tool for rapid prototyping of board games.
+
+## TODO
+* Hidden boards
+* Draw onto board
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var vulcanize = require('gulp-vulcanize');
+ 
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      livereload: true,
+      fallback: 'index.html',
+      port: 8080
+    }));
+});
+ 
+gulp.task('vulcanize', function() {
+  return gulp.src('index.html')
+    .pipe(vulcanize({
+      abspath: '.',
+      inlineScripts: true,
+      inlineCss: true,
+      inputUrl: '../../../../index.html'
+    }))
+    .pipe(gulp.dest('dist'));
+});
